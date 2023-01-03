905b7508293cb3430d1c327de3bcbdc83ba54fc5
Nico <jaylordiii@gmail.com>
2023-01-03 20:15:39 +0100

    initial

---
+++ build/CMakeFiles/tinyMP3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tinyMP3.dir/main.cpp.obj"
+  "libtinyMP3.dll.a"
+  "tinyMP3.exe"
+  "tinyMP3.exe.manifest"
+  "tinyMP3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tinyMP3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
